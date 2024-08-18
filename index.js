86934ddcb56c2fb4ac6e32acc1c4a2187a62c892
Premnishok-Muhilan <premnishok4@gmail.com>
2024-08-18 13:30:54 +0530

    Initial setup with ExpressJS

---
+++ index.js
@@ -0,0 +1,18 @@
+//BACKEND SERVER USING EXPRESS.JS
+//import the express module
+const express = require("express");
+//create an express application
+const app = express();
+//Define route handler
+//Avoiding this would result in "Cannot GET /" error
+app.get("/", (request, response) => {
+  response.send("Hello using experss JS");
+});
+//Just another route handler
+app.get("/new", (request, response) => {
+  response.send("/new using experss JS");
+});
+//start the server and listen on port
+app.listen(3001, () => {
+  console.log("Express server listening on port 3001");
+});
